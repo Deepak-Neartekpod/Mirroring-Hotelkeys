0d9b7b393f915f1ef86f18a0f0be33c384408041
Ratheesh-Karnan-1999 <ratheesh.karnan@neartekpod.com>
2025-02-13 12:27:23 +0530

    Availability UI Changes

--- src/data/data.js
+++ src/data/data.js
@@ -1,6 +1,25 @@
 const Data = [
   {
-    data: "1",
+    type: "Preferred Two Queen Beds",
+    rates: [
+      { label: "Best Available Rate", price: 214, tax: 29.96, available: 1 },
+      { label: "COMP", price: 0, tax: 0, available: 1 },
+    ],
+  },
+  {
+    type: "Classic King Bed",
+    rates: [
+      { label: "Best Available Rate", price: 179, tax: 25.06, available: 0 },
+      { label: "COMP", price: 0, tax: 0, available: 0 },
+    ],
+  },
+  {
+    type: "Classic King Bed Accessible",
+    rates: [
+      { label: "Best Available Rate", price: 179, tax: 25.06, available: 0 },
+      { label: "COMP", price: 0, tax: 0, available: 0 },
+    ],
   },
 ];
+
 export default Data;
